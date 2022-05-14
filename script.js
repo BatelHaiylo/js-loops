11d272b3f1b3992b69d9fdbedeadc6ed2845a89b
Batel Haiylo <inpo686@gmail.com>
2022-05-14 23:10:47 +0300

    11.5

---
+++ script.js
@@ -0,0 +1,224 @@
+// function printWellcome(){
+//     document.write("Hello World");
+// }
+// var i = 0
+// while(i<3){
+//     console.log(printWellcome());
+//     i++
+// }
+
+// function printMax(num1 , num2){
+//     for(i=0; i<2; i++){
+//         if (num1 >= num2){
+//             var max = num1
+//         }
+//         else{
+//             var max = num2
+//         }
+//     return max;
+//     }
+// }
+
+
+// function printSum(num1 , num2){
+//     var sum = num1+num2
+//     return sum
+// }
+
+// function numsSum(num){
+//     var sum = 0
+//     for(var times=0; num>times; times++){
+//         if( num>0){
+//             sum += num
+//         }
+//         num--;
+//     }
+//     return sum
+// }
+// console.log(numSun());
+
+
+// function numSum(i){
+// var sum=0
+// var num =+ prompt('enter'); i>0;i--
+// for(var i= num; i>0;i--){
+//     sum += i    
+// }
+// return sum
+// }
+// console.log(numSum())
+
+
+// var first =9
+// var second =8
+// function mixMatch(num1, num2){
+//     var num1 = first
+//     var num2 = second
+//         if(num1 != num2){
+//             var num3 = num1;
+//             if(num3 != num2){
+//                 num1 = num2;
+//                 if(num1 == num2){
+//                     num2 = num3
+//                 }
+//             }        
+//         }
+//     console.log(mixMatch());
+// }
+
+
+var first =3
+var second =7
+function mixMatch(){
+    var num1 = first
+    var num2 = second 
+    if(num1 != num2){
+        var num3 = num1;
+        if(num3 != num2){
+            num1 = num2;
+            if(num1 == num2){
+                num2 = num3;
+            }
+        }        
+    }
+    console.log(mixMatch())
+}
+
+
+// function numMin(num1, num2){
+//     if (num1>= num2){
+//         return num2
+//     }
+//     else{
+//         return num1
+//     }
+// }
+
+// function numMin(num1, num2, num3){
+//     if (num1< num2 && num1<num3){
+//         return num1
+//     }
+//     else if (num2< num1 && num2<num3){
+//         return num2
+//     }
+//     return num3
+// }
+
+// function numAvg(num, num, num){
+//     debugger
+//     var sum = 0
+//     for (i=0; i<3; i++){
+//         sum += num
+//     }
+//     var avg = sum/3
+//     return avg;
+// }
+
+// function numsSum (num1, num2, num3, num4){
+//     return num1 + num2 + num3 + num4;
+// }
+
+// function power (base, exponent){
+//     return base**exponent;
+// }
+// console.log(power())
+
+
+// function sumBack (num){
+//     num = num.toString()
+//     var sum = 0
+//         for(char of num){
+//             sum +=+ char
+//         }
+//     return sum
+// }
+
+// function fibonacci (num){
+//     num.lenght = num
+//     if num>num.lenght() 
+//     num = a +
+//     for 
+// }
+
+
+// function fibonacci (num){
+//     var fibonacciSeries = [0,1,1,2,3,5,8,13,21,34,55,89,144,233,377]
+//     for(i of fibonacciSeries){
+//         if(i<= num){
+//             return fibonacciSeries[i];
+//         }
+//     }
+// }
+
+// function getMax (num1, num2){
+//     if (num1 >= num2){
+//         return num1;
+//     }
+//     return num2;
+// }
+// function getMax (num1, num2){
+//     return (num1>=num2) ? num1 : num2 ;
+// }
+
+
+// function fizzBuzz (){
+//     if(typeof input != Number){
+//         return "input is NaN";
+//     }
+//     if ((input%3==0)&&(input%5==0)){
+//         return 'fizzBuzz';
+//     }
+//     if (input%3==0){
+//         return 'fizz';
+//     }
+//     if (input%5==0){
+//         return 'Buzz';
+//     }
+//     else{
+//         return input;
+//     }
+// }
+
+// function cheakSpeed (speed){
+//     var speedLimit = 70
+//     var points = 0
+
+//     if (speed<= speedLimit ){
+//         return "speed is ok"
+//     }
+//     if (speed == speedLimit+5){
+//         points +=1 
+//         return "you recive"+ points +"points" 
+//     }
+//     if (points == 12){
+//         return "your license is taken away"
+//     }
+// }
+
+// function multiplicationBoard (){
+//     for(var i=1; i<=10; i++){
+//         for(var j=0; j<=10; j++){
+//             document.write("i"+"."+i+"*"+j+"="+i*j+<br>)
+//         }
+//     }
+// }
+// multiplicationBoard()
+
+
+// function staredTraingle(num){
+//     var col = num
+//     var row = col/2</br>
+//     var star = "*"
+//     for (i=0; i<col; i++){
+//         for (j=0; j<row; j++){
+//             document.write(j*star);
+//         }
+//     }
+// }
+// staredTraingle(8)
+
+function maxMin (){
+    var number =+ prompt("enter a num")
+
+
+}
